bbd8e60d2ff7a286c24780d308295d6bc21f4c62
LorCat9 <lorenzo.catalli@gmail.com>
2019-05-04 17:59:07 +0200

    updated userForecast table

--- src/entity/UserForecast.ts
+++ src/entity/UserForecast.ts
@@ -4,8 +4,8 @@ import Forecast from "./Forecast";
 @Entity()
 export default class UserForecast {
     @PrimaryGeneratedColumn("uuid") id: string;
-    @PrimaryColumn({ unique: true }) email: string;
-    @PrimaryColumn({ unique: true }) nickname: string;
+    @Column({ unique: true }) email: string;
+    @Column({ unique: true }) nickname: string;
     @Column() region: string;
     @Column() latestVersion: number;
 
